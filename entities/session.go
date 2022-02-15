6702bbb2f8f6f91bf0a71d20a23cb42a051f9027
AlecSmith96 <rcsmith.alec@gmail.com>
2022-02-15 14:06:50 +0000

    added sessionHandler struct

--- entities/session.go
+++ entities/session.go
@@ -1,9 +1,12 @@
 package entities
 
 import (
+	"errors"
+	"net/http"
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Session struct {
@@ -11,4 +14,48 @@ type Session struct {
 	Name    string
 	From    time.Time
 	To      time.Time
+}
+
+type SessionCreate struct {
+	Name string `json:"name"`
+}
+
+func (s *Session) BeforeCreate(tx *gorm.DB) (err error) {
+	s.ID = uuid.New()
+	return nil
+}
+
+func (s *Session) Bind(r *http.Request) error  {
+	// Runs after unmarshalling is complete, do postprocessing
+	if s.Name == "" {
+		return errors.New("missing requried Name field")
+	}
+	return nil
+}
+
+func (s *Session) Render(w http.ResponseWriter, r *http.Request) error {
+	// Pre-processing before response is marshalled
+	return nil
+}
+
+// swagger:parameters createNewSession updateSession
+type SessionParamsWrapper struct {
+	// in:body
+	Body SessionCreate
+}
+
+// swagger:parameters getSession updateSession deleteSession
+type SessionId struct {
+	// in:path
+	SessionId string `json:"sessionId"`
+}
+
+// swagger:response SessionList
+type SessionList struct {
+	// in:body
+	Sessions []Session `json:"sessions"`
+}
+
+func (s *SessionList) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
